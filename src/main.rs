056d0a14cc038fb7f824217bb325562e25420f79
CBke <cbatdotcom@gmail.com>
2023-08-15 11:43:35 +0200

    simplify code

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,17 @@ fn main() {
     const WORD: &str = "hello";
     
     let array: Vec<String> = (0..(TERM_WIDTH - WORD.len()))
-        .map(|i| format!("{}{WORD}\r\n", " ".repeat(i)))
+        .map(|i| format!("{}{WORD}", " ".repeat(i)))
         .collect();
 
     loop {
-        array.iter().for_each(|x| print!("{x}"));
+        array.iter().for_each(|x| println!("{x}"));
 
         array
             .iter()
             .skip(1)
             .rev()
             .skip(1)
-            .for_each(|x| print!("{x}"));
+            .for_each(|x| println!("{x}"));
     }
 }
