5578c3bcfcb883da78c0b8090955c30f1a8f23ec
CBke <cbatdotcom@gmail.com>
2023-08-15 10:56:27 +0200

    Code Cleanup

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,20 @@ use std::io::{stdout, Write};
 fn main() {
     const TERM_WIDTH: usize = 241;
     const WORD: &str = "hello";
+    const FILL: &str = " ";
 
     let array: [String; TERM_WIDTH - WORD.len()] =
-        from_fn(|i| format!("{}{}\r\n", " ".repeat(i), WORD));
+        from_fn(|i| format!("{}{WORD}\r\n", FILL.repeat(i)));
 
     let mut lock = stdout().lock();
     loop {
-        array
-            .iter()
-            .for_each(|x| write!(lock, "{}", x).unwrap());
+        array.iter()
+            .for_each(|x| write!(lock, "{x}").unwrap());
 
-        array
-            .iter()
+        array.iter()
             .skip(1)
             .rev()
             .skip(1)
-            .for_each(|x| write!(lock, "{}", x).unwrap());
+            .for_each(|x| write!(lock, "{x}").unwrap());
     }
 }
